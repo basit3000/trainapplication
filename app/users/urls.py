1e06951528501cebee8f450c96799c349e71e81f
Muhammad Basit Zaheer <basit3000@yahoo.com>
2024-06-05 17:27:10 +0200

    feat: Implement authentication and profile page
    - Implement logout functionality
    - Add style.css
    - Base template update with URL patterns

--- app/users/urls.py
+++ app/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import UserRegisterView
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
